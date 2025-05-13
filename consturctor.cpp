94a8f90862d2eeeee76d8bab995fcd1fa37a063e
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2025-05-13 18:50:56 +0700

    constructor dengan parameter

--- consturctor.cpp
+++ consturctor.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa {
+class mahasiswa { //tanpa parameter
     public:
     mahasiswa();
 }; // constructor mahasiswa
 
+// constructor untuk menginisiasi
+// contructor itu ada dua bisa langsung di tulis bisa juga harus mengambil nama kelas terlebih dahulu
+// 
+
 mahasiswa :: mahasiswa (){
     cout << "Constructor terpanggil " << endl;
 
-};
+}; // definisi condtructor mahasiswa
 
 int main (){
     mahasiswa mhs;
     return 0;
-};
+}; //pembuatan objek
